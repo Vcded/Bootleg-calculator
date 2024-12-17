ada7f5efd7468fdf26b63f8d4618b85a729e2437
Timothy <160292745+Vcded@users.noreply.github.com>
2024-12-16 22:29:41 -0500

    Created a few prompts and the first function for the first operation

--- calculator/FileName.cpp
+++ calculator/FileName.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+int Addition(int a, int b);
+
+int Addition(int a, int b) {
+	return a + b;
+}
+
 int main(void) {
+	printf("\t---Shit calculatoor---");
+	printf("What ooperation would u like to perform: ");
+	printf("1. Addition");
+	printf("2. Subtraction");
+	printf("3. Multiplication");
+	printf("4. Division");
+	printf("5. Modulus");
+	printf("6. Square/CUbe of a number");
+	printf("7. Other options...");
 
 	return 0;
 }
+
+
+
