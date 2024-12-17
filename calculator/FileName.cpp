31f130c802a1968ef850fc3caeef26758fe805fd
Timothy <160292745+Vcded@users.noreply.github.com>
2024-12-16 22:33:27 -0500

    Added new lines after each prompt and made a function for subtraction

--- calculator/FileName.cpp
+++ calculator/FileName.cpp
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
 int Addition(int a, int b);
+int Subtraction(int a, int b);
 
 int Addition(int a, int b) {
 	return a + b;
 }
 
+int Subtraction(int a, int b) {
+	return a - b;
+}
+
 int main(void) {
 	printf("\t---Shit calculatoor---");
-	printf("What ooperation would u like to perform: ");
-	printf("1. Addition");
-	printf("2. Subtraction");
-	printf("3. Multiplication");
-	printf("4. Division");
-	printf("5. Modulus");
-	printf("6. Square/CUbe of a number");
-	printf("7. Other options...");
+	printf("\nWhat ooperation would u like to perform: ");
+	printf("\n1. Addition");
+	printf("\n2. Subtraction");
+	printf("\n3. Multiplication");
+	printf("\n4. Division");
+	printf("\n5. Modulus");
+	printf("\n6. Square/CUbe of a number");
+	printf("\n7. Other options...\n");
 
 	return 0;
 }
